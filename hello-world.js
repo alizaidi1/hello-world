05f92242fb100f1db2e0328d807b65319a61f76c
syed zeeshan ali zaidi <zaidi@debian>
2016-04-21 11:48:53 +0200

    zaidi

--- hello-world.js
+++ hello-world.js
@@ -3,13 +3,16 @@
 // convenience to get you started writing code faster.
 // Make sure to look at test.script.js--that should give you some hints about what is
 // expected here.
-
 var HelloWorld = function() {};
 
 HelloWorld.prototype.hello = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+	
+	if(typeof(input) === 'undefined'){
+		return 'Helloworld';
+	}else { return 'Hello'+ input ;}
+	
+	
+	
 };
-
-module.exports = HelloWorld;
+var bayay = new HelloWorld();
+console.log (bayay.hello("babababab"));
